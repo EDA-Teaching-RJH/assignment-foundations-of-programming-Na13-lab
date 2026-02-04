7ecc774f6261b55ca3589d68c76e505099669eca
Na13-lab <na13@kent.ac.uk>
2026-02-04 14:10:21 +0000

    Database

---
+++ fleet_manager.py
@@ -0,0 +1,6 @@
+def init_database():
+    names= ["William Regal", "Edward Teach", "Jack Sparrow", "Deanna Troi", "Zakaria Sheikh"]
+    ranks = ["Lieutenant", "Commander", "Captain", "Counselor", "Lieutenant"]
+    divisions = ["Security", "Command", "Command", "Counseling", "Operations"]
+    ids = [ "03", "02", "01", "05", "04"]
+    return names, ranks, divisions, ids 
